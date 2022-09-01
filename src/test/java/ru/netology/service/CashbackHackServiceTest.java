f40c7a5767d01a9891da9462b4bbf37a7f54ef04
Ulia95 <grebennikovaulia95@gmail.com>
2022-09-01 13:26:23 +0300

    TestNG

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,75 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+   public void calculateBonus() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 50;
+
+        int expected = 950;
+        int actual = cashbackHackService.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void calculateBonus2() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 10000;
+
+        int expected = 1000;
+        int actual = cashbackHackService.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void calculateBonus3() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1500;
+
+        int expected = 500;
+        int actual = cashbackHackService.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void calculateBonus4() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = -50;
+
+        int expected = 1050;
+        int actual = cashbackHackService.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void calculateBonus5() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 0;
+
+        int expected = 1000;
+        int actual = cashbackHackService.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void calculateBonus6() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int expected = 0;
+        int actual = cashbackHackService.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+}
